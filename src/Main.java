49d3835369676cb13fcc64cc9cad138dc0254df3
Ayush <ayushtamrakar.work@gmail.com>
2023-12-19 20:30:01 +0530

    Added constructor method to GermanShefard class.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,9 @@ import Inheritence.GermanShefard;
 
 public class Main {
     public static void main(String[] args){
-        GermanShefard gs = new GermanShefard();
-
-        gs.name = "Babu Khan";
-        gs.age = 3;
-        gs.color = "black";
-
+        GermanShefard gs = new GermanShefard("Babu Khan", 3, "black");
         gs.Running();
         gs.Eating();
-        System.out.println();
+        System.out.println("Name: " + gs.name + ", Age: " + gs.age + ", Color: " + gs.color);
     }
 }
