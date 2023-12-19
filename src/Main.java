bba8c3170bf96e03f0378ab0db21b0e714a3a69c
Ayush <ayushtamrakar.work@gmail.com>
2023-12-19 21:05:01 +0530

    Completed Encapsulation related task (Task 2)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import Encapsulation.BankCustomer;
 import Inheritence.GermanShefard;
 
 public class Main {
     public static void main(String[] args){
-        GermanShefard gs = new GermanShefard("Babu Khan", 3, "black");
-        gs.Running();
-        gs.Eating();
-        System.out.println("Name: " + gs.name + ", Age: " + gs.age + ", Color: " + gs.color);
+
+    //Inheritance related code (Task 1)
+//        GermanShefard gs = new GermanShefard("Babu Khan", 3, "black");
+//        gs.Running();
+//        gs.Eating();
+//        System.out.println("Name: " + gs.name + ", Age: " + gs.age + ", Color: " + gs.color);
+
+    //Encapsulation related code (Task 1)
+        BankCustomer cust = new BankCustomer("lokendra", 12345678, 1, 23, 'M', 9876543210L);
+
+        System.out.println("Id: "+ cust.getId() +"\nName: " + cust.getName() + "\nAccount Number: " + cust.getAccountNumber() + "\nGender: " + cust.getGender() + "\nAge: " + cust.getAge() + "\nPhone Number: "+ cust.getPhoneNumber());
     }
 }
