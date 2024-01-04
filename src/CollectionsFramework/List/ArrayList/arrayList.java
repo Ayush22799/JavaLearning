53d2ca4326c241ac27bec8665b86ba53e3a0ea6c
Ayush <ayushtamrakar.work@gmail.com>
2024-01-04 20:51:25 +0530

    Completed Map's Task 1

--- src/CollectionsFramework/List/ArrayList/arrayList.java
+++ src/CollectionsFramework/List/ArrayList/arrayList.java
@@ -1,13 +1,13 @@
 package CollectionsFramework.List.ArrayList;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import CollectionsFramework.List.LinkedList.Employee;
+
+import java.util.*;
 
 public class arrayList {
     public void task1(){
         System.out.println("\nTASK 1: \n");
-        List<Integer> arr = new ArrayList<Integer>();
+        List<Integer> arr = new ArrayList<>();
         for (int i = 5; i > 0; i--) {
             arr.add(i);
         }
@@ -20,7 +20,7 @@ public class arrayList {
 
     public void task2(){
         System.out.println("\nTASK 2: \n");
-        List<Integer> arr = new ArrayList<Integer>();
+        List<Integer> arr = new ArrayList<>();
         for (int i = 5; i > 0; i--) {
             arr.add(i);
         }
@@ -30,4 +30,48 @@ public class arrayList {
 
         System.out.println( "Sorted Array List is: " + arr);
     }
+
+    public void task3(){
+        System.out.println("\nTask 3: \n");
+
+        List<Employee> list = new LinkedList<>();
+
+        Employee<String, Integer> e1 = new Employee<>("Ayush", 24, 28000, "ASE (Accenture)");
+        Employee<String, Integer> e2 = new Employee("Lokendra", 23, 75000, "Analyst (Cognizant)");
+        Employee<String, Integer> e3 = new Employee("Faizal", 24, 26000, "Trainee (Mindtree)");
+        Employee<String, Integer> e4 = new Employee("Praful", 24, 0, "Berozgar");
+
+        list.add(e1);
+        list.add(e2);
+        list.add(e3);
+        list.add(e4);
+
+        System.out.println("Names of Employees are:");
+
+        Iterator<Employee> it = list.iterator();
+
+        while (it.hasNext()) {
+            Employee emp = it.next();
+            System.out.println(emp.getName());
+        }
+    }
+
+    public void task4() {
+        System.out.println("\nTask 4: \n");
+
+        List<Integer> list1 = new LinkedList<>();
+
+        for (int i = 0; i < 5; i++) {
+            list1.add(i + 1);
+        }
+
+        Iterator<Integer> integerIterator = list1.iterator();
+
+        while (integerIterator.hasNext()) {
+            System.out.println(integerIterator.next());
+        }
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
+import CollectionsFramework.Map.Map;
 import ObjectOrientedProgramming.Abstraction.SBIBank;
-import CollectionsFramework.List.LinkedList.Employee;
 import CollectionsFramework.List.ArrayList.arrayList;
 import ObjectOrientedProgramming.Encapsulation.BankCustomer;
 import ObjectOrientedProgramming.Inheritence.GermanShefard;
@@ -7,14 +7,11 @@ import ObjectOrientedProgramming.Polymorphism.MethodOverloading.Addition;
 import ObjectOrientedProgramming.Polymorphism.MethodOverriding.Child;
 import ObjectOrientedProgramming.Polymorphism.MethodOverriding.Parent;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args){
-      // Object Oriented Programming(OOPS) related tasks starts from here
-    /*
+    public static void main(String[] args) {
+        /* Object Oriented Programming(OOPS) related tasks starts from here */
+
+        /*
     Task 1:
       Inheritance();
     Task 2:
@@ -26,29 +23,33 @@ public class Main {
     /*
 
       /*Collection Framework related tasks starts from here */
-        List();
 
+//       List();
+        map();
     }
-    public static void Inheritance(){
+
+    public static void Inheritance() {
         GermanShefard gs = new GermanShefard("Babu Khan", 3, "black");
         gs.Running();
         gs.Eating();
         System.out.println("Name: " + gs.name + ", Age: " + gs.age + ", Color: " + gs.color);
     }
-    public static void Encapsulation(){
+
+    public static void Encapsulation() {
         BankCustomer cust = new BankCustomer("lokendra", 12345678, 1, 23, 'M', 9876543210L);
-        System.out.println("Id: "+ cust.getId() +"\nName: " + cust.getName() + "\nAccount Number: " + cust.getAccountNumber() + "\nGender: " + cust.getGender() + "\nAge: " + cust.getAge() + "\nPhone Number: "+ cust.getPhoneNumber());
+        System.out.println("Id: " + cust.getId() + "\nName: " + cust.getName() + "\nAccount Number: " + cust.getAccountNumber() + "\nGender: " + cust.getGender() + "\nAge: " + cust.getAge() + "\nPhone Number: " + cust.getPhoneNumber());
 
     }
-    public static void Abstraction(){
+
+    public static void Abstraction() {
         SBIBank sbi = new SBIBank();
-        System.out.println(sbi.rateOfInterest(50000,9,2));
+        System.out.println(sbi.rateOfInterest(50000, 9, 2));
     }
 
-    public static void Polymorphism(){
+    public static void Polymorphism() {
         Addition add = new Addition();
-        add.Sum(1,2);
-        add.Sum(1,2,3);
+        add.Sum(1, 2);
+        add.Sum(1, 2, 3);
 
         Parent p = new Parent();
         p.Eating();
@@ -57,51 +58,27 @@ public class Main {
         c.Eating();
     }
 
-    public static void List(){
+    public static void List() {
         arrayList arr = new arrayList();
 
         //Task 1:
         arr.task1();
+
         //Task 2:
         arr.task2();
 
         //Task 3:
-        System.out.println("\nTask 3: \n");
-
-        List<Employee> list = new LinkedList<Employee>();
-
-        Employee<String, Integer> e1 = new Employee<>("Ayush",24,28000,"ASE (Accenture)");
-        Employee<String, Integer> e2 = new Employee("Lokendra",23,75000,"Analyst (Cognizant)");
-        Employee<String, Integer> e3 = new Employee("Faizal",24,26000,"Trainee (Mindtree)");
-        Employee<String, Integer> e4 = new Employee("Praful",24,0,"Berozgar");
-
-        list.add(e1);
-        list.add(e2);
-        list.add(e3);
-        list.add(e4);
-
-        System.out.println("Names of Employees are:" );
-
-        Iterator<Employee> it = list.iterator();
-
-        while (it.hasNext()){
-            Employee emp = it.next();
-            System.out.println(emp.getName());
-        }
+        arr.task3();
 
         //Task 4:
-        System.out.println("\nTask 4: \n");
-
-        List<Integer> list1 = new LinkedList<>();
-
-        for (int i = 0; i < 5; i++) {
-            list1.add(i+1);
-        }
-
-        Iterator<Integer> integerIterator = list1.iterator();
-
-        while (integerIterator.hasNext()){
-            System.out.println(integerIterator.next());
-        }
-        }
+        arr.task4();
     }
+
+    public static void map() {
+
+        Map map = new Map();
+
+        //Task 1
+        map.Task1();
+    }
+}
