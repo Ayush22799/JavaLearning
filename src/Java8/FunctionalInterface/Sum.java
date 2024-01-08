b061e4d2d4e918c06df7636fe548d5b950f6b995
Ayush <ayushtamrakar.work@gmail.com>
2024-01-08 18:44:56 +0530

    Rearranged code for Java 8 Tasks

--- src/Java8/FunctionalInterface/Sum.java
+++ src/Java8/FunctionalInterface/Sum.java
@@ -1,4 +1,4 @@
-package Java8.LamdaExp;
+package Java8.FunctionalInterface;
 
 //Create a functional interface named Sum
 @FunctionalInterface
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import CollectionsFramework.Map.LearnMap;
 import CollectionsFramework.Queue.LearnQueue;
 import CollectionsFramework.Set.LearnSet;
 import CollectionsFramework.Stack.LearnStack;
-import Java8.LamdaExp.Sum;
+import Java8.LearnJava8;
 import ObjectOrientedProgramming.Abstraction.SBIBank;
 import CollectionsFramework.List.ArrayList.arrayList;
 import ObjectOrientedProgramming.Encapsulation.BankCustomer;
@@ -11,16 +11,9 @@ import ObjectOrientedProgramming.Polymorphism.MethodOverloading.Addition;
 import ObjectOrientedProgramming.Polymorphism.MethodOverriding.Child;
 import ObjectOrientedProgramming.Polymorphism.MethodOverriding.Parent;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class Main {
     public static void main(String[] args) {
 //         System.out.println("\n Object Oriented Programming(OOPS) related tasks starts from here\n");
-
 
 //    Task 1:
 //      Inheritance();
@@ -30,7 +23,6 @@ public class Main {
 //      Abstraction();
 //    Task 4:
 //      Polymorphism();
-
 
 //        System.out.println("\n ---------------------Collection Framework related tasks starts from here-----------------------------\n");
 
@@ -129,85 +121,11 @@ public class Main {
     }
     public static void java8(){
 
-        System.out.println("----LAMBDA TASK: \n");
+        LearnJava8 java = new LearnJava8();
+        java.Lambda();
+        java.Optional();
+        java.Stream();
 
-        //in main method use lambda function to give implementation of method
-        Sum s = (double a, double b) -> System.out.println(a+b);
-
-        //print sum of 98.2+12.8 using method of functional interface.
-        s.add(98.2, 12.8);
-
-        System.out.println("\n----OPTIONAL TASK: \n");
-
-        String name = "lokendra";
-
-        //CREATE A OPTIONAL of string & insert value "lokendra"
-        Optional<String> ifName = Optional.of(name);
-
-        //check if value is present using optional method and print on console.
-        if (ifName.isPresent()){
-            System.out.println("Lokendra is Present");
-        }
-        else System.out.println("Lokendra is Absent. Submit Rs5000 fine to Pawan Patidar");
-
-        System.out.println("\n----STREAM TASK: \n");
-
-        //CREATE A LINKEDLIST WITH VALUES 1,2,3,4,5
-        List<Integer> list = new LinkedList<>();
-        for (int i = 5; i >= 1 ; i--) {
-            list.add(i);
-        }
-        System.out.println("Initially list looks like: " + list.toString()+ "\n");
-
-        System.out.println("--MAP(): \n");
-
-        //Use map method of stream and add 1 to each value of linkedlist value and collect values of stream in list using collect(--) method of stream. then print the list.
-        List<Integer> finalList = list.stream().map(x -> x + 1).collect(Collectors.toList());
-        System.out.println("After adding stream's map() function, list looks like: "+ finalList.toString());
-
-
-        System.out.println("\n--Filter(): \n");
-
-        //FILTER ALL EVEN NUMBERS IN THIS LINKEDLIST AND collect values of stream in list using collect(--) method of stream. then print the list.
-        List<Integer> filterList = list.stream().filter(x-> x%2 ==0).collect(Collectors.toList());
-        System.out.println("After adding stream's filter() function, list looks like: "+ filterList.toString());
-
-        System.out.println("\n--Sorted(): \n");
-
-        //use sorted method of stream and collect values of stream in list using collect(--) method of stream and print list on console.
-        List<Integer> SortedList = list.stream().sorted().collect(Collectors.toList());
-        System.out.println("After adding stream's sort() function, list looks like: "+ SortedList.toString());
-
-        System.out.println("\n--forEach(): \n");
-
-        //use foreach method to print linkedlist values .
-        System.out.println("Using stream's forEach() function, Printing initial List: ");
-        list.stream().forEach(x-> System.out.println(x));
-
-        System.out.println("\n--flatMap(): \n");
-
-        // create 2d linkedlist
-
-        List<String> bakeryList = new LinkedList();
-
-        bakeryList.add("pasta");
-        bakeryList.add("donut");
-
-        List<String> productList = new LinkedList();
-
-        productList.add("tomato");
-        productList.add("pepper");
-
-        List<List<String>> grocerylist = new LinkedList<>();
-
-        grocerylist.add(bakeryList);
-        grocerylist.add(productList);
-
-        System.out.println("Initially 2D list looks like: \n"+ grocerylist.toString());
-
-        List<String> lst = grocerylist.stream().flatMap(l -> l.stream()).collect(Collectors.toList());
-
-        System.out.println("\nAfter adding stream's flatMap() function, list looks like: \n"+ lst.toString());
     }
 
 }
