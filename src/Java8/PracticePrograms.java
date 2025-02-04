3fdb69aa4259e19204897c277adb7061b84f70bd
Ayush Tamrakar <ayushtamrakar.work@gmail.com>
2025-02-04 19:34:43 +0530

    Added programs for REDUCE function in stream API

--- src/Java8/PracticePrograms.java
+++ src/Java8/PracticePrograms.java
@@ -14,20 +14,25 @@ public class PracticePrograms {
         System.out.println("2. Print all Courses Individually: ");
         individualCourses(courses);
 
-        System.out.println("3. Print Course that contains \"String\" ");
+        System.out.println("\n3. Print Course that contains \"String\" ");
         printCharOfString(courses);
 
-        System.out.println("4. Print courses whose name has at least 4 characters: ");
+        System.out.println("\n4. Print courses whose name has at least 4 characters: ");
         coursesWithFourDigit(courses);
 
         /* Questions for Map function */
-        System.out.println("___________ MAP Function _____________");
-        System.out.println("1. Print square of numbers: ");
+        System.out.println("\n___________ MAP Function _____________");
+        System.out.println("\n1. Print square of numbers: ");
         squareOfNumbers(numbers);
-        System.out.println("2. Print cubes of odd numbers: ");
+        System.out.println("\n2. Print cubes of odd numbers: ");
         cubesOfOddNumbers(numbers);
-        System.out.println("3. Print length of name of each course: ");
+        System.out.println("\n3. Print length of name of each course: ");
         numberOfCharacters(courses);
+
+        /* Questions for Reduce function */
+        System.out.println("\n___________ Reduce Function _____________");
+        System.out.println("\n1. Print the sum of total elements present in list: ");
+        sumOfElementsInList(numbers);
     }
 
     public static void printOddNumbers(List<Integer> list){
@@ -55,5 +60,8 @@ public class PracticePrograms {
 
     public static void numberOfCharacters(List<String> courses){
         courses.stream().map(String::length).forEach(System.out::println);
+    }
+    public static void sumOfElementsInList(List<Integer> numberList){
+        System.out.println(numberList.stream().reduce(0, Integer::sum));
     }
 }
